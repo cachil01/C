6e33e296125659437c0017006ff55988d5a1c624
Christakis Achilleos <cachil01@b103ws15.in.cs.ucy.ac.cy>
2018-10-09 01:28:40 +0300

    finito, eginan submit

--- part3/sedcmp.c
+++ part3/sedcmp.c
@@ -1,10 +1,39 @@
+/**
+* @file sedcmp.c
+* @brief This is a program compares two strings and calculates the distance between 
+*
+*
+*
+*
+*
+* @author Christakis Achilleos
+* @bug No known bugs. 
+*
+*
+*
+*
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// This is a macro for comparing 3 numbers and finding the minimum
 #define MIN(X,Z,Y) ( (X) < (Z) ? ( (X) < (Y) ? (X) : (Y)) : ( (Z) < (Y) ? (Z) : (Y)) )
 
 int sed(char*, char*, int, int);
+
+
+/**
+* @brief This is the main funtion. 
+*
+* This function calls the sed function that calculates the minimum number of steps.
+* 
+*
+*
+* @param argc The number of arguments given through the command prompt
+* @param argv The arguments given through the command prompt
+* @return returns 0 when finished
+*/
 int main(int argc, char *argv[]){
 
 
@@ -61,69 +90,6 @@ int main(int argc, char *argv[]){
 	memcpy( table1, temp1, size1);
 	memcpy( table2, temp2, size2);
 
-
-	/*
-	while((*dp1 = fgetc(fp1)) != EOF){
-		
-		count1 ++;
-		printf("char is added: %c\n", *dp1);
-		//printf("count1: %d\n", count1);
-
-		if(count1 == size1){
-
-			
-			dp1 = NULL;
-			temp1 = (char *) malloc(sizeof(char)*size1*2);
-			if(temp1 == NULL){
-				printf("Failed to allocate memory.Exiting.\n");
-				exit(EXIT_FAILURE);
-			}
-
-			memcpy(temp1, table1, (size1)   * sizeof(char));
-			free(table1);
-			table1 = temp1;
-			dp1 = table1 + (size1);
-			size1*=2;
-			
-			temp1 =NULL;
-		}
-		dp1++;
-	}*/
-	
-	/*
-	while((*dp2 = fgetc(fp2)) != EOF){
-
-		count2 ++;
-
-		//printf("count2: %d\n", count2);
-
-		if(count2 == size2){
-
-			size2*=2;
-			dp2 = NULL;
-			temp2 = (char *) malloc(sizeof(char)*size2);
-			if(temp2 == NULL){
-				printf("Failed to allocate memory.Exiting.\n");
-				exit(EXIT_FAILURE);
-			}
-			//printf("%c\n", *table2 );
-			memcpy(temp2, table2, (size2/2)  * sizeof(char));
-			free(table2);
-			table2 = temp2;
-			dp2 = table2 + size2/2;
-			temp2 =NULL;
-		}
-		dp2++;
-
-
-
-
-	}
-	*/
-	//*dp1 = '\0';
-	//*dp2 ='\0';
-
-	printf("size1 = %d , size2 = %d\n", size1, size2);
 	
 	
 	
@@ -134,57 +100,23 @@ int main(int argc, char *argv[]){
 	printf("Minimum edits required to conver is %d\n", num);
 	
 	
-	
-
-
-
 	return 0;
 
 
 }
 
+/**
+* @brief This function uses
+*
+* This function calls the sed function that calculates the minimum number of steps.
+* 
+*
+*
+* @param argc The number of arguments given through the command prompt
+* @param argv The arguments given through the command prompt
+* @return returns 0 when finished
+*/
 
-int sed(char *table1, char *table2, int size1, int size2){
-	
-	int temp ;
-
-	if (size1 == 0){
-
-		return size2;
-	}
-
-	if (size2 ==0){
-
-		return size1;
-	}
-
-	if(table1[size1 - 1] == table2[size2 -1]){
-		
-		temp =0;
-	}
-	else {
-		temp = 1;
-	}
-	
-	int res1 = sed(table1 , table2, size1, size2-1);
-	int res2 = sed(table1, table2, size1-1,size2);
-	int res3 = sed(table1,table2, size1-1, size2-1);
-
-	if (res1 < res2){
-
-		if(res1 < res3){
-			return 1+res1;
-		}
-		return temp+res3;
-
-
-	}
-	if(res2 < res3){
-		return 1 +res2;
-	}
-	return temp+res3;
-
-}
 
 int sedC(char * str1, char * str2, int m, int n) 
 { 
@@ -199,27 +131,25 @@ int sedC(char * str1, char * str2, int m, int n)
     { 
         for ( j=0; j<=n; j++) 
         { 
-            // If first string is empty, only option is to 
-            // isnert all characters of second string 
+            //Here if the first string is empty then we have to insert
+	    //all characters from the second string
             if (i==0) 
                 dp[i][j] = j;  // Min. operations = j 
   
-            // If second string is empty, only option is to 
-            // remove all characters of second string 
+            //Here if the second string is empty then we have to insert
+	    //all characters from the first string
             else if (j==0) 
-                dp[i][j] = i; // Min. operations = i 
+                dp[i][j] = i; 
   
-            // If last characters are same, ignore last char 
-            // and recur for remaining string 
+            //Here if the last part is the same then ignore it and proceed with the rest
             else if (str1[i-1] == str2[j-1]) 
                 dp[i][j] = dp[i-1][j-1]; 
   
-            // If the last character is different, consider all 
-            // possibilities and find the minimum 
+            // In case the last is different then check which option is minimum
             else
-                dp[i][j] = 1 + MIN(dp[i][j-1],  // Insert 
-                                   dp[i-1][j],  // Remove 
-                                   dp[i-1][j-1]); // Replace 
+                dp[i][j] = 1 + MIN(dp[i][j-1],  // Here we check for inserting a char
+                                   dp[i-1][j],  // Here we check for deleting a char
+                                   dp[i-1][j-1]); // Here we check for replacing a character. 
         } 
     } 
   
