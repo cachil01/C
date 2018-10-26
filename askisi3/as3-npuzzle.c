c47c662ef394124dd251f6d9ab8e4be2bbd88c1e
Christakis Achilleos <cachil01@b103ws5.in.cs.ucy.ac.cy>
2018-10-27 00:02:38 +0300

    i can now read the puzzle

--- askisi3/as3-npuzzle.c
+++ askisi3/as3-npuzzle.c
@@ -1,7 +1,10 @@
-//#include "tree.h"
+#include "TREE_NODE.h"
 //#include "frontier_node.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int **readFile(char*filein, int size);
 
 int main(int argc, char *argv[]){
 
@@ -14,37 +17,55 @@ int main(int argc, char *argv[]){
 	char *filein = argv[2];
 	char *fileout = argv[3];
 	printf("Break: 2\n");
-
+	int size = determineSize(filein);
 	//TREE_NODE *p = NULL;
-	int **puzzle = NULL;
-	printf("Break: 2.5\n");
+	int **puzzle = readFile(filein, size);
+	
 
-	int size = readFile(filein, &puzzle);	
 	printf("Break: 7\n");
 
-	int **row = NULL;
-	int *collumn = NULL;
-	row= puzzle;
+
 	
 	printf("testing the readfile\n\n");
 	
 	int i,k;
 	for(i = 0;i<size;i++){
 		
-		collumn = *row;
+		
 		for(k =0;k<size;k++){
-			printf("%d ",*collumn);
-			collumn++;
+			printf("%d ",puzzle[i][k]);
+			
 		}
 		printf("\n");
-		row++;
+		
 	}
 
 	
 
 }
 
-int readFile(char *filein, int ***puzzle){
+int determineSize(char *filein){
+
+	FILE *fp = NULL;
+	fp = fopen(filein, "r");
+	if(fp == NULL){
+		printf("Error in opening file\n");
+		exit(EXIT_FAILURE);
+	}	
+	
+	int count;
+	char *tmp =(char*) malloc(sizeof(char)*100);
+	while(fgets(tmp, 100, fp) != NULL){
+		count++;
+	}
+	free(tmp);
+
+	return count;
+
+}
+
+
+int ** readFile(char *filein, int size){
 
 	FILE *fp = NULL;
 	fp = fopen(filein, "r");
@@ -56,15 +77,7 @@ int readFile(char *filein, int ***puzzle){
 		exit(EXIT_FAILURE);
 	}
 	
-	printf("Break: 3\n");
-
-
-	
-	int count;
-	char * tmp = (char*) malloc(sizeof(char)*100);
-	while(fgets(tmp, 100, fp) != NULL){
-		count++;
-	}
+	int **puzzle = (int**)malloc(sizeof(int*)*size);
 
 
 	printf("Break: 4\n");
@@ -72,41 +85,44 @@ int readFile(char *filein, int ***puzzle){
 
 	int **temp = NULL;
 	int *temp2 = NULL;
-	*puzzle  = (int **) malloc(sizeof(int*)*count);
-	temp = *puzzle;
+	puzzle  = (int **) malloc(sizeof(int*)*size);
+	temp = puzzle;
 	int i;
 	
 	printf("Break: 5\n");
-	for(i=0;i<count;i++){
-		*temp =(int*)malloc(sizeof(int)*count);
+
+	for(i=0;i<size;i++){
+		*temp =(int*)malloc(sizeof(int)*size);
 		temp++; 
 
 	}
 	 
-	rewind(fp);
-	temp = *puzzle;
+
+	temp = puzzle;
 	printf("Break: 6\n");
 
-
+	char *tmp = (char*) malloc(sizeof(char)*100);
 	char *hp = NULL;
 	while(fgets(tmp, 100, fp) != NULL){
 		hp = tmp;
+		temp2 = *temp;
 		while(*hp!= '\0'){
-			temp2 = *temp;
-			if(*hp != '\t' && *hp!= '\n'){
+			
+			if(*hp != '\t' && *hp!= '\n' && *hp != '\0'){
 				*temp2 = *hp - '0';
+
 				temp2++;	
 			}
 			hp++;
 
 		}
 
-		
+		temp++;		
 	}
 
 	
 	
-	return count;
+	return puzzle;
 }
 
 
